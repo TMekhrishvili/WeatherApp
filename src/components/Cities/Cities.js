38b5c94bd8b682cf7d5a3e67e6ea16a4ecd01bfa
Tamar Mekhrishvili <tamar.mekhrishvili@medsoft.ge>
2020-10-11 02:10:33 +0400

    not important

--- src/components/Cities/Cities.js
+++ src/components/Cities/Cities.js
@@ -4,25 +4,28 @@ import { Row, Col } from 'antd';
 
 const Cities = ({ cities }) => {
     const { score, setScore } = useContext(SettingsContext);
-    const { temp, setTemp } = useState(false);
+    const [ temp, setTemp ] = useState(false);
     const image = { width: '200px', margin: '20px', cursor: 'pointer' };
+    console.log(cities);
     const handleClick = () => {
         setTemp(true);
     }
 
     return (
-        <Row justify="center">
-            {cities.map((value, index) => {
-                const photo = `https://d13k13wj6adfdf.cloudfront.net/urban_areas/${value.city}.jpg`;
-                return (
-                    <Col key={index} span={4}>
-                        <img onClick={handleClick} style={image} src={photo} alt="city" />
-                        <h1 align="center">{value.city.charAt(0).toUpperCase() + value.city.slice(1)}</h1>
-                        <h2 align="center">{value.temp}</h2>
-                    </Col>
-                );
-            })}
-        </Row>
+        cities.length > 0 && (
+            <Row justify="center">
+                {cities.map((value, index) => {
+                    const photo = `https://d13k13wj6adfdf.cloudfront.net/urban_areas/${value.city}.jpg`;
+                    return (
+                        <Col key={index} span={4}>
+                            <img onClick={handleClick} style={image} src={photo} alt="city" />
+                            <h1 align="center">{value.city.charAt(0).toUpperCase() + value.city.slice(1)}</h1>
+                            <h2 align="center">{temp && value.temp}</h2>
+                        </Col>
+                    );
+                })}
+            </Row>
+        )
     );
 }
 
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,41 +7,43 @@ import Cities from '../Cities';
 const { Content } = Layout;
 
 const Home = () => {
-    //const [cities, setCities] = useState([]);
+    useEffect(() => {
+        setCitiesAndTemperature([]);
+        getTemperature();
+    }, [])
+    const [cities, setCities] = useState([]);
+    const [citiesAndTemperature, setCitiesAndTemperature] = useState([]);
     const { difficulty, history, setHistory } = useContext(SettingsContext);
-    
-    const tmp = () => {
-        //setHistory([...history, cities])
-    }
-    const getCities = () => {
-        const citiesArray = ["mumbai", "rome", "milan", "tbilisi", "baku", "cologne", "vienna", "prague", "dubai", "athens", "new-york", "paris", "berlin", "barcelona", "madrid"];
-        const shuffled = citiesArray.sort(() => 0.5 - Math.random());
-        let selected = shuffled.slice(0, difficulty);
-        return selected;
+
+    const nextStep = () => {
+        setHistory([...history, cities]);
     }
 
     const getTemperature = () => {
-        const citiesArray = getCities();
-        const citiesAndTemp = [];
-        citiesArray.map(city => {
+        const citiesArray = ["mumbai", "rome", "milan", "tbilisi", "baku", "cologne", "vienna", "prague", "dubai", "new-york", "paris", "berlin", "barcelona", "madrid"];
+        const shuffled = citiesArray.sort(() => 0.5 - Math.random());
+        const selected = shuffled.slice(0, difficulty);
+        selected.map( city => {
             const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=15111268423bd37aca3f9c5d5b9d86f0`;
             fetch(url)
                 .then((res) => res.json())
                 .then((data) => {
-                    citiesAndTemp.push({ city: city, temp: data.main.temp });
+                    setCitiesAndTemperature([...citiesAndTemperature, { city: city, temp: data.main && data.main.temp }]);
                 });
         });
-        return citiesAndTemp;
+        setCities(selected);
     }
 
     return (
-        <Layout>
-            <Content>
-                <Divider orientation="center">Guess the temperature</Divider>
-                <Cities cities={getTemperature()} />
-                <Button onClick={tmp} type="primary" style={{ display: 'block', margin: '30px', alignContent: 'right' }}>Continue</Button >
-            </Content>
-        </Layout>
+        citiesAndTemperature.length > 0 && (
+            <Layout>
+                <Content>
+                    <Divider orientation="center">Guess the temperature</Divider>
+                    <Cities cities={citiesAndTemperature} />
+                    <Button onClick={nextStep} type="primary" style={{ display: 'block', margin: '30px', alignContent: 'right' }}>Continue</Button >
+                </Content>
+            </Layout>
+        )
     );
 }
 
